777a5116e1b37985f9c9a09ceb33d28aae5e7aeb
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2026-02-02 08:24:17 -0700

    fix: loading UI

--- examples/sso-interop-portal/frontend/src/components/Home/HomeTab.tsx
+++ examples/sso-interop-portal/frontend/src/components/Home/HomeTab.tsx
@@ -45,48 +45,68 @@ export function HomeTab({
       className="tab-content active"
       id="home-tab"
     >
-      {isMounted && (!passkeyCredentials || !accountAddress) ? (
-        <div id="setup-section">
-          <div className="card">
-            <div
-              id="home-get-started"
-              className="card-title"
-            >
-              {t("home.getStarted")}
-            </div>
-            <div
-              id="home-setup-wallet"
-              className="card-subtitle"
-            >
-              {t("home.setupWallet")}
-            </div>
-          </div>
-
-          {/* <!-- Step 1: Create Passkey --> */}
-          <CreatePasskey
-            passkeyCredentials={passkeyCredentials}
-            setPasskeyCredentials={setPasskeyCredentials}
-          />
-
-          {/* <!-- Step 2: Deploy Account --> */}
-          <DeployAccount
-            passkeyCredentials={passkeyCredentials}
-            setAccountAddress={setAccountAddress}
-          />
-        </div>
-      ) : (
+      {isMounted ? (
         <>
-          {accountAddress && shadowAccount ? (
+          {!passkeyCredentials || !accountAddress ? (
+            <div id="setup-section">
+              <div className="card">
+                <div
+                  id="home-get-started"
+                  className="card-title"
+                >
+                  {t("home.getStarted")}
+                </div>
+                <div
+                  id="home-setup-wallet"
+                  className="card-subtitle"
+                >
+                  {t("home.setupWallet")}
+                </div>
+              </div>
+
+              {/* <!-- Step 1: Create Passkey --> */}
+              <CreatePasskey
+                passkeyCredentials={passkeyCredentials}
+                setPasskeyCredentials={setPasskeyCredentials}
+              />
+
+              {/* <!-- Step 2: Deploy Account --> */}
+              <DeployAccount
+                passkeyCredentials={passkeyCredentials}
+                setAccountAddress={setAccountAddress}
+              />
+            </div>
+          ) : (
             <WalletInfo
               accountAddress={accountAddress}
-              shadowAccount={shadowAccount}
+              shadowAccount={shadowAccount!}
               balance={balance}
               setActiveTab={setActiveTab}
             />
-          ) : (
-            <div className="alert alert-error">Error: account address found but passkey is missing.</div>
           )}
         </>
+      ) : (
+        <div className="spinner">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z"
+            >
+              <animateTransform
+                attributeName="transform"
+                dur="0.75s"
+                repeatCount="indefinite"
+                type="rotate"
+                values="0 12 12;360 12 12"
+              />
+            </path>
+          </svg>
+        </div>
       )}
     </div>
   );
